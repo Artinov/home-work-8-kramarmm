e523888ac62c2bbe7083ed7ff2e104b96bf7d348
kramarmm <kramar.mariya@mail.ru>
2017-02-19 12:11:23 +0200

    Added a body of functions

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 function setText(selector, text) {
-	/*
-		`selector` is string with CSS-like selector
-		`text` - is just string with text we want to change
-	*/
+	var selector = document.querySelector(selector);
+		selector.innerHTML = text;
 
+		// `selector` is string with CSS-like selector
+		// `text` - is just string with text we want to change
 	// Write your code here
-}
+};
 
 
 function setTextAll(selector, text) {
 	// Write your code here
-}
+	var selector = document.querySelectorAll(selector);
+	for(i = 0; i < selector.length; i++){
+		selector[i].innerHTML = text;
+	};
+};
 
 
 setText("p", "wasn't that hard, yeah ?");
